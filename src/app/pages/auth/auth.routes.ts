e85d073e08f2a21f47ba90edc0e0930b3a28f145
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-04 14:29:12 +0100

    add auth ayout

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AuthComponent } from './auth.component';
 
 export const AuthenticaionRoutest: Routes = [
   {
     path: '',
-    component: SignInComponent,
+    component: AuthComponent,
     children: [
+      { path: '', component: SignInComponent },
       {
         path: 'login',
         component: SignInComponent,
