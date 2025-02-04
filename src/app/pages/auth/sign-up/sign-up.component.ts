d8a65b7e6d4348c5cb39ba790ab33441698766dd
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-04 15:20:00 +0100

    add success state of sign up

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -9,22 +9,26 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import * as AuthSelectors from '../../../store/auth/auth.selectors';
 import * as AuthActions from '../../../store/auth/auth.actions';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
   private readonly store = inject(Store);
+  private successSubscription!: Subscription;
+
 
   signUpForm: FormGroup;
 
   loading$ = this.store.select(AuthSelectors.selectAuthLoading);
   error$ = this.store.select(AuthSelectors.selectAuthError);
+  success$ = this.store.select(AuthSelectors.selectIsAuthenticated);
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.signUpForm = this.fb.group({
@@ -32,6 +36,20 @@ export class SignUpComponent {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  ngOnInit(): void {
+    this.successSubscription = this.success$.subscribe((success) => {
+      if (success) {
+        this.signUpForm.reset();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.successSubscription) {
+      this.successSubscription.unsubscribe();
+    }
   }
 
   onSubmit(): void {
