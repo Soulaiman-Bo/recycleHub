3932f8c95f70e842b5ddb093c60fa0bcb40e51a9
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-04 14:31:08 +0100

    add register user and stor it in localstorage

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import * as AuthSelectors from '../../../store/auth/auth.selectors';
+import * as AuthActions from '../../../store/auth/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+  private readonly store = inject(Store);
 
+  signUpForm: FormGroup;
+
+  loading$ = this.store.select(AuthSelectors.selectAuthLoading);
+  error$ = this.store.select(AuthSelectors.selectAuthError);
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      this.store.dispatch(AuthActions.signUp(this.signUpForm.value));
+    }
+  }
 }
