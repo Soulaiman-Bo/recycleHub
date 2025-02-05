3e5ba87dfa73fe9f44c7fda60153a163e62f00d2
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-05 09:37:31 +0100

    add login form functionality

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { selectAuthError } from '../../../store/auth/auth.selectors';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../../store/auth/auth.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
 export class SignInComponent {
+  private readonly store = inject(Store);
 
+  loginForm: FormGroup;
+
+  authError$ = this.store.select(selectAuthError);
+
+  loginSuccess = false; // Track success message
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.store.dispatch(AuthActions.login({ email, password }));
+
+      this.authError$.subscribe((error) => {
+        if (!error) {
+          this.loginSuccess = true;
+          setTimeout(() => this.router.navigate(['/']), 1500);
+        }
+      });
+    }
+  }
 }
