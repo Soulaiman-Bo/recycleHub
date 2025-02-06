396f4f6715e2031a9be6900ac2db7100ac72b95a
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-06 12:30:49 +0100

    added dashboard layout

---
+++ src/app/pages/dashboard/layout/layout.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
+import { TopbarComponent } from '../../../shared/components/topbar/topbar.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, TopbarComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css',
+})
+export class LayoutComponent {
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+}
