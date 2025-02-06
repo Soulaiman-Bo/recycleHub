9f55338133d8875eb75692ec6e5e9905d73543a3
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-06 09:36:14 +0100

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
-    path: "auth",
-    loadChildren : () => import("./pages/auth/auth.routes").then((m)=> m.AuthenticaionRoutest)
-  }
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./pages/auth/auth.routes').then((m) => m.AuthenticaionRoutest),
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
 ];
