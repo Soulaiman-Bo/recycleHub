5fd2ec3bdd407fce87a71f2cd1d6582ab7611a57
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-06 12:29:00 +0100

    added top bar of the dashboard

---
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.css',
+})
+export class TopbarComponent {
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+}
