ad36289c52c9533cc1d3e10a3f8896f90123cc8b
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-09 00:21:22 +0100

    add optional city of user

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,10 +1,8 @@
-
 export interface User {
-    id?: string
-    username: string
-    email: string
-    role: string
-    password?: string
+  id?: string;
+  username: string;
+  email: string;
+  role: string;
+  password?: string;
+  city?: string;
 }
-
-
