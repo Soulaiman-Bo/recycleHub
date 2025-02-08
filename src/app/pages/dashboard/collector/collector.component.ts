cb2ae126b5a79c40f516ca6796f2d876f1a4eaa3
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-09 00:23:43 +0100

    expot the CollectorCollectionTableComponent to the dashboard of collector

--- src/app/pages/dashboard/collector/collector.component.ts
+++ src/app/pages/dashboard/collector/collector.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
+import { CollectorCollectionTableComponent } from '../../../shared/components/collector-collection-table/collector-collection-table.component';
 
 @Component({
   selector: 'app-collector',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CollectorCollectionTableComponent],
   templateUrl: './collector.component.html',
   styleUrl: './collector.component.css'
 })
