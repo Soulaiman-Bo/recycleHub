33d16e43234e78356bd224ac04b1f0c302602eeb
SOULAIMAN BOUHLAL <bouhlal.soulaiman@gmail.com>
2025-02-09 23:12:20 +0100

    fixed rediction after login in

--- src/app/pages/dashboard/dashboard.routes.ts
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -4,12 +4,19 @@ import { CollectorComponent } from './collector/collector.component';
 import { IndividualComponent } from './individual/individual.component';
 import { HomeComponent } from '../home/home.component';
 import { RoleGuard } from '../../core/guards/role.guard';
+import { RoleRedirectGuard } from '../../core/guards/role-redirect.guard';
 
 export const DashboardRoutes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+
     children: [
+      {
+        path: '',
+        component: LayoutComponent,
+        canActivate: [RoleRedirectGuard],
+      },
       {
         path: 'collector',
         component: CollectorComponent,
